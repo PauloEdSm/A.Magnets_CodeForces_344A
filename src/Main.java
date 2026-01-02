9b171ba7546721bda88a96f25774ba680c66c105
paulodev <pauloeduardotroll9646@gmail.com>
2026-01-02 15:35:21 -0300

    Final version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     static void main(String[] args) {
-        int [] magnets = new int[2];
+        Scanner scanner = new Scanner(System.in);
+        int magnetCounts = scanner.nextInt();
+        String currentMagnetOrientation = "";
+        String prevMagnetOrientation ="";
+        int groupCounts = 0;
+        for (int j = 0; j < magnetCounts; j++) {
+            currentMagnetOrientation = scanner.next();
+            groupCounts = (!currentMagnetOrientation.equals(prevMagnetOrientation) || j ==0)
+                    ?groupCounts+1:groupCounts;
+
+            prevMagnetOrientation = currentMagnetOrientation;
+
+        }
+        System.out.println(groupCounts);
+
     }
 }
